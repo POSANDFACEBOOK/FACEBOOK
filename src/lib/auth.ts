491c1c1b0f3785b47e3982f278053fc7a75d9881
naiwansook <your@email.com>
2026-03-30 17:26:52 +0700

    add pages_read_engagement scope

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,7 +7,7 @@ export const authOptions = {
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
       authorization: {
         params: {
-          scope: 'ads_management,ads_read,pages_show_list,pages_read_engagement',
+          scope: 'ads_management,ads_read,pages_show_list,pages_read_engagement,pages_manage_ads',
           redirect_uri: 'https://fb-ads-manager.vercel.app/api/auth/callback/facebook',
         },
       },
